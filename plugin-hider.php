7c18675a949f2e91a3052fa74793a3582fc226bf
Ali Mahryn <alymahryn@gmail.com>
2024-08-31 11:06:34 +0500

    Update plugin-hider.php

--- plugin-hider.php
+++ plugin-hider.php
@@ -9,6 +9,7 @@ Author: Ali Mahryn
 function hide_plugins($plugins) {
     // List of plugins to hide (use the plugin's folder name)
     $hidden_plugins = array(
+        'plugin-hider', // Hides this plugin
         'Your-plugin-name',
         // Add more plugin folder names here if needed
     );
