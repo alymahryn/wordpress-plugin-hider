e3ab5f90c87761083758a6fe1ebadc042fb1b2c6
Ali Mahryn <alymahryn@gmail.com>
2024-09-01 10:21:51 +0500

    Update plugin-hider.php

--- plugin-hider.php
+++ plugin-hider.php
@@ -2,20 +2,20 @@
 /*
 Plugin Name: Plugin Hider
 Description: Hides specified plugins from the WordPress plugins list
-Version: 1.0
+Version: 1.1
 Author: Ali Mahryn
 */
 
 function hide_plugins($plugins) {
-    // List of plugins to hide (use the plugin's folder name)
+    // List of plugins to hide (use the plugin's folder name and/or main file name)
     $hidden_plugins = array(
-        'plugin-hider', // Hides this plugin
-        'Your-plugin-name',
-        // Add more plugin folder names here if needed
+        'your-plugin-folder' => 'your-plugin.php',
+        
+        // Add more plugins here if needed, following the 'folder' => 'main-file.php' pattern
     );
 
-    foreach ($hidden_plugins as $plugin) {
-        $plugin_path = $plugin . '/' . $plugin . '.php';
+    foreach ($hidden_plugins as $folder => $main_file) {
+        $plugin_path = $folder . '/' . $main_file;
         if (isset($plugins[$plugin_path])) {
             unset($plugins[$plugin_path]);
         }
@@ -24,3 +24,21 @@ function hide_plugins($plugins) {
     return $plugins;
 }
 add_filter('all_plugins', 'hide_plugins');
+
+// Debug function to print active plugins
+function debug_plugin_hider() {
+    if (current_user_can('manage_options') && isset($_GET['debug_plugin_hider'])) {
+        echo '<div class="notice notice-info is-dismissible">';
+        echo '<p><strong>Plugin Hider Debug Information:</strong></p>';
+        echo '<p>Active Plugins:</p>';
+        echo '<pre>';
+        print_r(get_option('active_plugins'));
+        echo '</pre>';
+        echo '<p>All Plugins:</p>';
+        echo '<pre>';
+        print_r(get_plugins());
+        echo '</pre>';
+        echo '</div>';
+    }
+}
+add_action('admin_notices', 'debug_plugin_hider');
